af94c69d197051466e4bae64bc6ba398a1df3e16
Egor <dragun.egor@gmail.com>
2016-11-13 14:59:50 +0300

    Добавлена страница регистрации(над обговорить будет еще) и добавлены ajax-запросы для страниц авторизации и регистрации

---
+++ src/main/webapp/WEB-INF/pages/mainAuthorization/js/main.js
@@ -0,0 +1,14 @@
+var xhr = new XMLHttpRequest();
+
+var body = 'name=' + encodeURIComponent(name) +
+    '&password=' + encodeURIComponent(password);
+
+xhr.open("POST", '/submit', true)
+xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+
+xhr.onreadystatechange = function() {
+    if (xhr.readyState != 4)
+        console.log("reguest 400");
+    return;
+}
+xhr.send(body);
