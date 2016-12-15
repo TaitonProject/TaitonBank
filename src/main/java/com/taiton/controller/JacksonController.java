60d521f062e19ca0f5e61e115ae40a1582cd4566
Taiton <taiton252005@gmail.com>
2016-12-15 03:57:24 +0300

    AngularJs подключил, resources папку добавил

--- src/main/java/com/taiton/controller/JacksonController.java
+++ src/main/java/com/taiton/controller/JacksonController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -19,11 +20,17 @@ public class JacksonController {
     @Autowired
     private UserDao userDao;
 
-    @GetMapping("/listUsers")
-    public ResponseEntity getUserList(){
+    /*@GetMapping("/listUsers")
+    public @ResponseBody ResponseEntity getUserList(){
         List<UserEntity> userEntityList = userDao.findAll();
         if(userEntityList.isEmpty())
             return new ResponseEntity(HttpStatus.NO_CONTENT);
         return new ResponseEntity(userEntityList, HttpStatus.OK);
+    }*/
+
+    @GetMapping("/listUsers")
+    public @ResponseBody List<UserEntity> getUserList(){
+        List<UserEntity> userEntityList = userDao.findAll();
+        return userEntityList;
     }
 }
