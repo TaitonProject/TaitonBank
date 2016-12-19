745ac7fe0a55bf08425a1d034add8e293faee25b
Egor <dragun.egor@gmail.com>
2016-12-19 12:00:29 +0300

    angularjs json проба два, вродь чтот норм

--- src/main/webapp/resources/js/controllers/UserController.js
+++ src/main/webapp/resources/js/controllers/UserController.js
@@ -5,9 +5,12 @@
  * UserController
  * @constructor
  */
+var myApp=angular.module('myApp',[]);
 var UserController = function($scope, $http) {
         $http.get('/listUsers.json').success(function (response) {
                 $scope.users = response;
         });
 };
+
+
 
