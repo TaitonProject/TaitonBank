f3e7a50b8fb2fe7f19826c0a75f9754b63b3f562
Egor <dragun.egor@gmail.com>
2017-01-02 18:38:16 +0300

    глянь роли

--- src/main/java/com/taiton/controller/PhoneController.java
+++ src/main/java/com/taiton/controller/PhoneController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Created by egordragun on 26.12.16.
  */
 @Controller
-@RequestMapping("/phone")
+@RequestMapping("/pay")
 public class PhoneController {
-    @RequestMapping()
+    @RequestMapping("/phone")
     public String getPhonePage() {
         return "pay/phone";
     }
