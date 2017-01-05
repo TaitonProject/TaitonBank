d9a52f63a610b24a8a68030591a5387a4159098e
Taiton <taiton252005@gmail.com>
2017-01-05 22:09:25 +0300

    Подправил контроллер регистрации с ангуляра Настройки бд подправил (кодировка не работает)

--- src/main/webapp/resources/templates/registration/js/controllers/UserRegistrationController.js
+++ src/main/webapp/resources/templates/registration/js/controllers/UserRegistrationController.js
@@ -10,44 +10,45 @@ UserRegistrationController = function ($scope, $http) {
 
     $scope.user = {};
 
+    $scope.fetchUsersList = function () {
+        $scope.resetError();
+        $http.get('/registration/registration.json').success(function (response) {
+            $scope.users = response;
+        }).error(function () {
+            $scope.setError('беда в предосталвнии списка пользователей')
+        });
+    };
 
     $scope.addNewUser = function (user) {
         $scope.resetError();
         $http.post('/registration/addUser', user).success(function () {
             $scope.fetchUsersList();
-            $scope.user.username = '';
-            $scope.user.password = '';
-            $scope.user.confirmPassword = '';
-            $scope.user.isBlocked = false;
-            $scope.user.roleByRoleIdRole = null;
-        }).error(function (status) {
-            if(status === 400){
-                $scope.setError('Ошибка! Проверьте введенные данные!')
-            }
-            $scope.setError('Не удалось зарегестрировать пользователя')
+            $scope.user = {
+                username: '',
+                password: '',
+                confirmPassword: '',
+                info : {
+                    firstName: '',
+                    secondName: '',
+                    sureName: '',
+                    passportNumber: ''
+                }
+            };
+        }).error(function () {
+            $scope.setError('беда при добавлении пользователя');
         })
     };
-
-/*    $scope.fetchRoleList = function () {
-        $scope.resetError();
-        $http.get('/registration/listRoles.json').success(function (response) {
-            $scope.roles = response;
-            $scope.selectedOption = $scope.roles[0];
-        }).error(function () {
-            $scope.setError('беда в предосталвнии списка ролей')
-        });
-    };*/
 
     $scope.setError = function (message) {
         $scope.error = false;
         $scope.errorMessage = message;
     };
 
-    $scope.resetError = function() {
+    $scope.resetError = function () {
         $scope.error = false;
         $scope.errorMessage = '';
     };
 
-    // $scope.fetchRoleList();
+    $scope.fetchUsersList();
 };
 
