e94e5bc6d4177e1c3f69b71b638b6bdca48463b9
Egor <dragun.egor@gmail.com>
2017-01-10 23:24:21 +0300

    123

--- src/main/webapp/resources/templates/home/js/filters.js
+++ src/main/webapp/resources/templates/home/js/filters.js
@@ -9,3 +9,16 @@ AppFilters.filter('interpolate', ['version', function (version) {
         return String(text).replace(/\%VERSION\%/mg, version);
     }
 }]);
+
+AppFilters.controller('mainController', function($scope) {
+
+$scope.submitForm = function() {
+
+    // check to make sure the form is completely valid
+    if ($scope.userForm.$valid) {
+        alert('our form is amazing');
+    }
+
+}
+
+});
