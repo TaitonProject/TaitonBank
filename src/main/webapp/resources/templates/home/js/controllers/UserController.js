945a7c7ff7396c1c4296678c9280982652622110
Taiton <taiton252005@gmail.com>
2017-01-23 14:48:27 +0300

    Подправил пустые контроллеры

--- src/main/webapp/resources/templates/home/js/controllers/UserController.js
+++ src/main/webapp/resources/templates/home/js/controllers/UserController.js
@@ -1,10 +1,3 @@
 UserController = function($scope, $http) {
-    $scope.fetchUserList = function () {
-        $http.get('/editing/listUsers.json').success(function (listUsers) {
-            $scope.users = listUsers;
-        });
-    };
-
-    $scope.fetchUserList();
 };
 
